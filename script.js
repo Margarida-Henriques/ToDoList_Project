711a0fc7a46fbbd0b0d7907927be4066c5a0a472
Margarida Henriques <margaridahenriques934@gmail.com>
2024-10-05 15:38:16 +0100

    TO-DO to DONE

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const addButton = document.getElementById('addTask');
 const taskInput = document.getElementById('taskInput');
 const taskList = document.getElementById('taskList');
+const taskDoneList = document.getElementById('taskDoneList');
 
 loadTasks();
 
@@ -11,7 +12,7 @@ function addTask() {
     const task = taskInput.value.trim()
 
     if (task) {
-        creatTaskElement(task);
+        createTaskElement(task);
         taskInput.value = '';
 
         saveTasks()
@@ -21,11 +22,12 @@ function addTask() {
 
 }
 
+//Input Button
 addButton.addEventListener('click', addTask)
 
 
-
-function creatTaskElement(task) {
+//Create TO-DO List Tasks 
+function createTaskElement(task) {
 
     //Create element
     const listItem = document.createElement('li');
@@ -33,21 +35,21 @@ function creatTaskElement(task) {
     //Creating text
     const taskText = document.createElement('span');
     taskText.textContent = task;
-    taskText.className = 'taskText'
+    taskText.className = 'taskText';
 
-    //Creating Option Div
-    const optionsContainer = document.createElement('div')
-    optionsContainer.className = 'optionsContainer'
+    //Creating Option Div (done / delete)
+    const optionsContainer = document.createElement('div');
+    optionsContainer.className = 'optionsContainer';
 
     //Creating an Done Button
-    const doneButton = document.createElement('button')
-    doneButton.textContent = '✔'
-    doneButton.className = 'doneButton'
+    const doneButton = document.createElement('button');
+    doneButton.textContent = '✔';
+    doneButton.className = 'doneButton';
 
     //Creating an Delete Button
-    const deleteButton = document.createElement('button')
-    deleteButton.textContent = 'X'
-    deleteButton.className = 'deleteButton'
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'X';
+    deleteButton.className = 'deleteButton';
 
     // Add elements to the DOM
     listItem.appendChild(taskText);
@@ -57,28 +59,61 @@ function creatTaskElement(task) {
     taskList.appendChild(listItem);
 
 
-    //Delete element clicked on
+    //Delete Button clicked on
     deleteButton.addEventListener('click', function () {
         taskList.removeChild(listItem);
-        saveTasks()
+        saveTasks();
+    })
+
+    //Done Button clicked on
+    doneButton.addEventListener('click', function () {
+        createDoneTaskElement(listItem.querySelector('.taskText').textContent);
+
+        //Remove from To-Do list
+        taskList.removeChild(listItem);
+        saveTasks();
     })
 
 }
 
-//Create a list and Add to Local Storage
+//Send TO-DO task to DONE list
+function createDoneTaskElement(taskText) {
+
+    const doneTask = document.createElement('li');
+    doneTask.className = 'doneTask';
+
+    const doneTaskText = document.createElement('span');
+    doneTaskText.textContent = taskText;
+    doneTask.className = 'doneTaskText';
+
+    doneTask.appendChild(doneTaskText);
+    taskDoneList.appendChild(doneTask);
+
+}
+
+
+//Save to-do and done tasks to Local Storage
 function saveTasks() {
     let tasks = [];
+    let tasksDone = [];
 
     taskList.querySelectorAll('.taskText').forEach(function (item) {
         tasks.push(item.textContent.trim());
     });
 
+    taskDoneList.querySelectorAll('.doneTaskText').forEach(function (item) {
+        tasksDone.push(item.textContent.trim());
+    });
+
     localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem('tasksDone', JSON.stringify(tasksDone));
 }
 
 //Load tasks when refreshing page
 function loadTasks() {
     const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    const tasksDone = JSON.parse(localStorage.getItem('tasksDone')) || [];
 
-    tasks.forEach(creatTaskElement);
+    tasks.forEach(createTaskElement);
+    tasksDone.forEach(createDoneTaskElement);
 }
